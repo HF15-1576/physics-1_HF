54b344cdf4fa191bbba32690973a6bc2ebb56c5e
HF15-1576 <66247898+HF15-1576@users.noreply.github.com>
2025-09-04 12:08:04 -0600

    Uhh Stinky?

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -8,25 +8,51 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
-const unsigned int TARGET_FPS = 50;
+const unsigned int TARGET_FPS = 50; // Target frames per second
+float dt = 1.0f / TARGET_FPS; // Delta time (time per frame)
 float time = 0;
-int main()
+float x = 650;
+float y = 500;
+float frequency = 1.0f;
+float amplitude = 100.0f;
+
+void update()
 {
-    InitWindow(1200, 800, "Physics-1");
-    SetTargetFPS(TARGET_FPS);
+		dt = 1.0f / TARGET_FPS;
+        time += dt;
 
-    while (!WindowShouldClose())
-    {
+		x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+		y = y + (cos(time * frequency)) * frequency * amplitude * dt;
+}
+
+void draw()
+{
         BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
+            ClearBackground(BLACK);
+            DrawText("Uhh Stinky", 440, 250, 50, DARKPURPLE);
 
 
-            time += 1;
-            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+            GuiSliderBar(Rectangle{ 60, 770, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 30);
+
+			DrawCircle(x, y, 80, PINK);
+
 
 
         EndDrawing();
+
+}
+
+
+int main()
+{
+    InitWindow(1200, 800, "GAME2005-Hunter_Faichney-101547907");
+    SetTargetFPS(TARGET_FPS);
+
+	while (!WindowShouldClose()) //Calls towards update and draw methods
+    {
+		update();
+		draw();
+
     }
 
     CloseWindow();
