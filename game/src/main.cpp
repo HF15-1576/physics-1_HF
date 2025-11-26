e9ca2558ae104b53d4986dfd9d7c99e4e041f0e3
HF15-1576 <66247898+HF15-1576@users.noreply.github.com>
2025-11-25 23:24:20 -0500

    Lab 4

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,33 +1,57 @@
-/*
-This project uses the Raylib framework to provide us functionality for math, graphics, GUI, input etc.
-See documentation here: https://www.raylib.com/, and examples here: https://www.raylib.com/examples.html
-*/
-
 #include "raylib.h"
 #include "raymath.h"
 #include <vector>
+#include <cassert>
+
+enum ColliderType
+{
+    COLLIDER_TYPE_INVALID,
+    COLLIDER_TYPE_CIRCLE,
+    COLLIDER_TYPE_HALF_SPACE
+    //COLLDER_BOX <-- not implemented yet
+};
+
+union Collider
+{
+    struct
+    {
+        float radius;
+    } circle;
+
+    struct
+    {
+        Vector2 normal;
+    } half_space;
+
+    struct
+    {
+        Vector2 extents;
+    } box;
+};
 
 struct PhysicsBody
 {
     Vector2 position = Vector2Zeros;
     Vector2 velocity = Vector2Zeros;
-	float drag = 0.0f; 
-    float invMass = 1.0f;
-	float gravityScale = 1.0f; 
 
+    float drag = 1.0f;
+    float inv_mass = 1.0f;
+    float gravity_scale = 1.0f;
+
+    ColliderType collider_type = COLLIDER_TYPE_INVALID;
+    Collider collider{};
     bool collision = false;
-
-    float radius = 0.0f;
-
+    //Color color = MAGENTA;// Now colored based on collision status
 };
 
 // Physics Simulation
 struct PhysicsWorld
 {
-    Vector2 gravity = { 0.0f, 150.0f };
+    Vector2 gravity = { 0.0f, 9.81f };
     std::vector<PhysicsBody> entities;
 };
 
+// LE3 TODO -- Complete this function!
 bool CircleCircle(Vector2 pos1, float rad1, Vector2 pos2, float rad2)
 {
     // Circle should turn red when overlapping once this function is implemented correctly.
@@ -38,36 +62,56 @@ bool CircleCircle(Vector2 pos1, float rad1, Vector2 pos2, float rad2)
     return distanceSq <= (radiusSum * radiusSum);
 }
 
+
+// LE4 TODO -- Complete this function!
+bool CircleHalfSpace(Vector2 pos_circle, float rad, Vector2 pos_half_space, Vector2 normal)
+{
+    Vector2 ab = pos_circle - pos_half_space;
+    float proj = Vector2DotProduct(ab, normal);
+    return proj <= rad;
+}
+
 int main()
 {
-    InitWindow(1200, 800, "(GAME2005)_Hunter-Faichney");
+    PhysicsWorld world;
+    PhysicsBody* entity = nullptr;
+
+    // Static circle
+    world.entities.push_back({});
+    entity = &world.entities.back();
+    entity->position = { 200.0f, 400.0f };
+    entity->gravity_scale = 0.0f;
+    entity->collider_type = COLLIDER_TYPE_CIRCLE;
+    entity->collider.circle.radius = 20.0f;
+
+    // Static half-space
+    world.entities.push_back({});
+    entity = &world.entities.back();
+    entity->position = { 350.0f, 450.0f };
+    entity->gravity_scale = 0.0f;
+    entity->collider_type = COLLIDER_TYPE_HALF_SPACE;
+    entity->collider.half_space.normal = Vector2UnitY * -1.0f;
+    entity->collider.half_space.normal = Vector2Rotate(Vector2UnitX, -45.0f * DEG2RAD);
+    // Simply rotate the normal if you'd like it to change directions!
+
+    // Dynamic circle
+    world.entities.push_back({});
+    entity = &world.entities.back();
+    entity->collider_type = COLLIDER_TYPE_CIRCLE;
+    entity->collider.circle.radius = 20.0f;
+
+    InitWindow(800, 800, "Physics-1");
     SetTargetFPS(60);
-
-	PhysicsWorld world;
-    world.entities.push_back({}); 
-    
-    // Static Circle
-	PhysicsBody* circle = &world.entities.back();
-	circle->position = { 600.0f, 400.0f };
-	circle->radius = 40.0f;
-	circle->gravityScale = 0.0f;
-
-    // Moving Circle 
-	world.entities.push_back({});
-	circle = &world.entities.back();
-	circle->radius = 30.0f;
-	circle->gravityScale = 0.0f;
-
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
-        circle->position = GetMousePosition();
+        entity->position = GetMousePosition();
 
         // Motion loop
         for (size_t i = 0; i < world.entities.size(); i++)
         {
             PhysicsBody& e = world.entities[i];
-            Vector2 acc = world.gravity * e.gravityScale;
+            Vector2 acc = world.gravity * e.gravity_scale;
 
             e.velocity += acc * dt;             // v = a * t
             e.position += e.velocity * dt;      // p = v * t
@@ -83,18 +127,74 @@ int main()
             {
                 PhysicsBody& a = world.entities[i];
                 PhysicsBody& b = world.entities[j];
-                bool collision = CircleCircle(a.position, a.radius, b.position, b.radius);
+                assert(a.collider_type != COLLIDER_TYPE_INVALID && b.collider_type != COLLIDER_TYPE_INVALID);
+                bool collision = false;
+
+                if (a.collider_type == COLLIDER_TYPE_CIRCLE &&
+                    b.collider_type == COLLIDER_TYPE_CIRCLE)
+                {
+                    collision = CircleCircle(
+                        a.position, a.collider.circle.radius,
+                        b.position, b.collider.circle.radius
+                    );
+                }
+                else if (
+                    a.collider_type == COLLIDER_TYPE_CIRCLE &&
+                    b.collider_type == COLLIDER_TYPE_HALF_SPACE)
+                {
+                    collision = CircleHalfSpace(
+                        a.position, a.collider.circle.radius,
+                        b.position, b.collider.half_space.normal
+                    );
+                }
+                else if (
+                    a.collider_type == COLLIDER_TYPE_HALF_SPACE &&
+                    b.collider_type == COLLIDER_TYPE_CIRCLE)
+                {
+                    collision = CircleHalfSpace(
+                        b.position, b.collider.circle.radius,
+                        a.position, a.collider.half_space.normal
+                    );
+                }
+
                 a.collision |= collision;
                 b.collision |= collision;
             }
         }
 
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(WHITE);
         for (const PhysicsBody& e : world.entities)
         {
-            DrawCircleV(e.position, e.radius, e.collision ? RED : GREEN);
+            Color color = e.collision ? RED : GREEN;
+            if (e.collider_type == COLLIDER_TYPE_CIRCLE)
+            {
+                DrawCircleV(e.position, e.collider.circle.radius, color);
+            }
+            else if (e.collider_type == COLLIDER_TYPE_HALF_SPACE)
+            {
+                // "Flip" the normal to determine the direction of the half-space
+                Vector2 direction = { -e.collider.half_space.normal.y, e.collider.half_space.normal.x };
+                Vector2 p0 = e.position + direction * 1000.0f;
+                Vector2 p1 = e.position - direction * 1000.0f;
+                DrawLineEx(p0, p1, 5.0f, color);
+                DrawLineEx(e.position, e.position + e.collider.half_space.normal * 50.0f, 5.0f, GOLD);
+            }
         }
+
+        // Hard-coded visuals for LE4:
+        Vector2 circle_pos = world.entities[2].position;
+        Vector2 half_space_pos = world.entities[1].position;
+        Vector2 normal = world.entities[1].collider.half_space.normal;
+
+        // AB = B - A
+        Vector2 to_circle = circle_pos - half_space_pos;
+        float proj = Vector2DotProduct(to_circle, normal);
+        DrawLineEx(half_space_pos, half_space_pos + to_circle, 5.0f, MAGENTA);
+        DrawCircleV(half_space_pos + normal * proj, 10.0f, PINK);
+        DrawLineEx(half_space_pos, half_space_pos + normal * proj, 4.0f, PINK);
+        // if proj is less than the radius of the circle, there's collision!
+
         EndDrawing();
     }
 
